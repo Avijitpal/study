af802662364ab481c6a5e12c50fba1f1d3d83d5b
Avijit <avijitpal4343@gmail.com>
2024-12-25 14:08:17 +0530

    Callback

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("hello avijit")
+//sqaure
+//cube
+// Callback is passing an argumrnt as a patameter.
+
+function cube(n){
+    const val = n*n*n;
+    return val;
+ }
+
+
+ function sqaure(n){
+    const val = n*n;
+    return val;
+ }
+function cal(a, b , callback){
+    console.log( callback)
+    const value1 = callback(a)
+    const value2 = callback(b);
+    return value1
+}
+
+const ans = cal(2,3,sqaure)
+console.log(ans)
+
+
+
