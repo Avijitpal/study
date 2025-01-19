9982f7af08f42ed0d73b2997b51f548c1bbb5fc8
Avijit <avijitpal4343@gmail.com>
2025-01-19 10:34:16 +0530

    revision

--- callback.js
+++ callback.js
@@ -1,36 +1,59 @@
-function orderFood(){
-     return new Promise((resolve,reject)=>{
-        let food = false;
-        if(food){
-            setTimeout(()=>{
-                console.log("Food is Ready");
-                resolve()
-             },2000)
-        }
-        else{
-           reject("Sorry the resturent is closed")
-        }
+// function orderFood(){
+//      return new Promise((resolve,reject)=>{
+//         let food = false;
+//         if(food){
+//             setTimeout(()=>{
+//                 console.log("Food is Ready");
+//                 resolve()
+//              },2000)
+//         }
+//         else{
+//            reject("Sorry the resturent is closed")
+//         }
         
-     })
+//      })
+// }
+
+
+// function notifyCustomer(){
+//     console.log("Your food is on the way !!!!")
+// }
+// function handleError(error){
+//     console.log(error)}
+
+
+// async function ProcessOrder() {
+//     try{
+//       await orderFood();
+//       notifyCustomer()
+//     }catch(error){
+//         handleError(error);
+//     }
+// }
+
+
+// ProcessOrder();
+
+
+
+
+
+
+function fetchData(callback){
+    setTimeout(()=>{
+          const data = {
+             name:"Avijit",
+             age: 26
+          }
+          callback(data)
+    },3000)
 }
 
 
-function notifyCustomer(){
-    console.log("Your food is on the way !!!!")
-}
-function handleError(error){
-    console.log(error)
-}
+fetchData(function(data){
+    console.log(data)
+})
 
 
-async function ProcessOrder() {
-    try{
-      await orderFood();
-      notifyCustomer()
-    }catch(error){
-        handleError(error);
-    }
-}
 
 
-ProcessOrder();
