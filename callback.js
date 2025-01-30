8ab647349be21b078870ae84bc8aad498a078e8a
Avijit <avijitpal4343@gmail.com>
2025-01-30 21:40:59 +0530

    Study

--- callback.js
+++ callback.js
@@ -1,38 +1,13 @@
-// function orderFood(){
-//      return new Promise((resolve,reject)=>{
-//         let food = false;
-//         if(food){
-//             setTimeout(()=>{
-//                 console.log("Food is Ready");
-//                 resolve()
-//              },2000)
-//         }
-//         else{
-//            reject("Sorry the resturent is closed")
-//         }
-        
-//      })
-// }
+function doSomthing(callback){
+    console.log("it is executing")
+  setTimeout(()=>{
+   callback()
+  },3000)
+}
 
 
-// function notifyCustomer(){
-//     console.log("Your food is on the way !!!!")
-// }
-// function handleError(error){
-//     console.log(error)}
+function sayHello(){
+    console.log("Hello Brother")
+}
 
-
-// async function ProcessOrder() {
-//     try{
-//       await orderFood();
-//       notifyCustomer()
-//     }catch(error){
-//         handleError(error);
-//     }
-// }
-
-
-// ProcessOrder();
-
-
-
+doSomthing(sayHello);
